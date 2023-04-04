9287eb82075563c005a1c2e903c6b6e2f413be9b
verybadgag <memel_esmel@hotmail.fr>
2023-04-04 11:16:48 +0200

    INIT

--- src/main/java/com/memel/TheSpoon/entities/Personne.java
+++ src/main/java/com/memel/TheSpoon/entities/Personne.java
@@ -1,4 +1,51 @@
 package com.memel.TheSpoon.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "personne")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Personne {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idPersonne", nullable = false)
+    private long id;
+
+    @Column(length = 50)
+    private String nom;
+
+    @Column(length = 50)
+    private String prenom;
+
+    @Column(length = 50)
+    private String adresse;
+
+    @Column(length = 50)
+    private String ville;
+
+    @Column(length = 50)
+    private String codePostal;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "personneReserveation",
+            joinColumns = { @JoinColumn(name = "idPersonne") },
+            inverseJoinColumns = { @JoinColumn (name = "idReservation")}
+    )
+    private List<Reservation> reservations;
+
+    @ManyToOne
+    @JoinColumn(name = "id_type_personne")
+    private TypePersonne typePersonne;
+
 }
--- src/main/java/com/memel/TheSpoon/entities/TypePersonne.java
+++ src/main/java/com/memel/TheSpoon/entities/TypePersonne.java
@@ -1,4 +1,28 @@
 package com.memel.TheSpoon.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "type_personne")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class TypePersonne {
+
+    @Id
+    @Column(name = "id_type_personne")
+    private Long id;
+
+    @Column(name = "libelle")
+    private String libelle;
+
 }
