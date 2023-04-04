4770f45c22e881fd3d1b3247bd2e8bb35ac835a5
verybadgag <memel_esmel@hotmail.fr>
2023-04-04 09:13:53 +0200

    Initial commit

---
+++ src/main/java/com/memel/TheSpoon/TheSpoonApplication.java
@@ -0,0 +1,13 @@
+package com.memel.TheSpoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSpoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSpoonApplication.class, args);
+	}
+
+}
