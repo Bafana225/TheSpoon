099bec3d640696b14a08292f9072a4c9d327f6a3
verybadgag <memel_esmel@hotmail.fr>
2023-04-05 14:23:13 +0200

    INIT

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,20 @@
 
+CREATE TABLE IF NOT EXISTS restaurant (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(50),
+  adresse VARCHAR(50),
+  nb_couverts INT,
+  accessibilite_pmr BOOLEAN,
+  prix_moyen DOUBLE
+);
 
-INSERT INTO restaurant (nom, adresse, nbCouverts, accessibilitePmr, prixMoyen) VALUES ('Le Petit Bistrot', '10 rue du Moulin', 30, true, 25.0);
-INSERT INTO restaurant (nom, adresse, nbCouverts, accessibilitePmr, prixMoyen) VALUES ('Chez Paul', '5 avenue des Champs', 50, false, 35.0);
-INSERT INTO restaurant (nom, adresse, nbCouverts, accessibilitePmr, prixMoyen) VALUES ('La Belle Epoque', '15 rue des Roses', 40, true, 45.0);
+INSERT INTO restaurant (nom, adresse, nb_couverts, accessibilite_pmr, prix_moyen)
+VALUES ('Le Petit Bistrot', '10 rue du Moulin', 30, true, 25.0);
+
+INSERT INTO restaurant (nom, adresse, nb_couverts, accessibilite_pmr, prix_moyen)
+VALUES ('Chez Paul', '5 avenue des Champs', 50, false, 35.0);
+
+INSERT INTO restaurant (nom, adresse, nb_couverts, accessibilite_pmr, prix_moyen)
+VALUES ('La Belle Epoque', '15 rue des Roses', 40, true, 45.0);
 
 
